08ee71a6b2ad6cabad3dbc355af284edf5c09884
AmeeerMaviaKhan <157229386+AmeeerMaviaKhan@users.noreply.github.com>
2024-01-26 18:53:05 +0500

    Add files via upload

---
+++ Example 3.py
@@ -0,0 +1,10 @@
+# Simple python program to understand elif statment
+number= int(input("Enter the number?"))
+if number==10:
+    print("Ther given number is equals to 10")
+elif number==50:
+    print('Ther given number is equal to 50');
+elif number==100:
+    print("The given number is equal to 100");
+else:
+    print("The given number is not equal to 10, 50 or 100");
