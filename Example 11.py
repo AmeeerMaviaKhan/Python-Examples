057214b6e9032fbd32e143a4c1332d4e881208b0
AmeeerMaviaKhan <157229386+AmeeerMaviaKhan@users.noreply.github.com>
2024-01-27 20:20:09 +0500

    Update Example 11.py

--- Example 11.py
+++ Example 11.py
@@ -1,56 +1,36 @@
+Example 1:
 import math
-
 x = math.sqrt (64)
-
 print (x)
 
-
-
+Example 2:
 x = math.ceil (91.4)
-
 y = math.floor (91.4)
-
 print(x)
 print(y)
 
-
-
+Example 3:
 import math
-
-x = math.
-pi
+x = math.pi
 print (x)
 
-
-
+Example 4:
 import datetime
-
 x = datetime.datetime.now()
-
 print(x)
 
-
-
+Example 5:
 import datetime
-
 x = datetime.datetime.now()
-
 print(x.year)
-
 print(x.strftime("%A"))
 
-
-
+Example 6:
 import datetime
-
 x = datetime.datetime(2020,5,17)
-
 print(x)
 
-
-
+Example 7:
 import datetime
-
 x = datetime.datetime(2018,6,1)
-
 print(x.strftime("%B"))
