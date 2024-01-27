91a165ea3ad3199d608ace6635a34a2a4c358890
AmeeerMaviaKhan <157229386+AmeeerMaviaKhan@users.noreply.github.com>
2024-01-27 20:09:38 +0500

    Update Example 5.py

--- Example 5.py
+++ Example 5.py
@@ -1,4 +1,4 @@
 # Simple Python program to understand the if statment
-num = int(input("Enter the nimber:"))
+num = int(input("Enter the number:"))
 if num%10 == 0:
     print("The Given is an even number")
